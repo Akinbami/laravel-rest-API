48d586abba9370749dd5ea672db64e4b94acb441
BABABUSOLA <queenbusola1@gmail.com>
2020-08-11 20:59:30 +0100

    Added phpunit test for external book API

--- tests/Feature/BookAPITest.php
+++ tests/Feature/BookAPITest.php
@@ -22,7 +22,6 @@ class BookAPITest extends TestCase
     public function test_can_get_all_external_books()
     {
         $response = $this->get('api/external-books');
-
         $response->assertStatus(200)
                 ->assertJson([
                         'status' => 'success',
